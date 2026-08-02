33b318dc653e548a3ea82baf4357b01edff85f93
Iván Pereira Muñoz <elchininet@gmail.com>
2026-08-02 18:49:38 +0200

    Migrate project to ESM

--- aliases.js
+++ aliases.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const tsconfig = require('./tsconfig');
+import path from 'node:path';
+import tsconfig from './tsconfig.json' with { type: "json" };
 
 const { compilerOptions: { paths } } = tsconfig;
 const aliasReg = (str) => str.replace(/^(.*)\/\*$/, '$1');
-module.exports = Object.keys(paths).reduce(
-    (obj, a) => (obj[aliasReg(a)] = path.resolve(__dirname, aliasReg(paths[a][0])), obj),
+export default Object.keys(paths).reduce(
+    (obj, a) => (obj[aliasReg(a)] = path.resolve(aliasReg(paths[a][0])), obj),
     {}
 );
